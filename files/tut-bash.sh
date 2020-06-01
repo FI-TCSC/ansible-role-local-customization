38befd51364cc9fc2ef025f580cdbdb66f0f7a67
Mika Anttila <16833294+mantti@users.noreply.github.com>
2020-06-01 10:20:14 +0300

    Added some ENV-variables for common FGCI ground

--- files/tut-bash.sh
+++ files/tut-bash.sh
@@ -1,5 +1,9 @@
 # Some local customization
 export WORK=/scratch
 
+# For Common FGCI environment
+export WRKDIR="$WORK/$USERNAME"
+export PROJ="/lustre"
+
 # Adding anaconda to PATH
 export PATH="/home/opt/anaconda3/bin:$PATH"
