af379d14d8ae57b1772a1970f42738faacdc0ff5
Mika Anttila <16833294+mantti@users.noreply.github.com>
2020-09-03 15:11:14 +0300

    Some environment variables to easen usage

--- files/tut-bash.sh
+++ files/tut-bash.sh
@@ -4,6 +4,8 @@ export WORK=/scratch
 # For Common FGCI environment
 export WRKDIR="$WORK/$USERNAME"
 export PROJ="/lustre"
+export NVME="/lustre/nvme"
+export SING_FLAGS=' -B /scratch:/scratch '
 
 # Adding anaconda to PATH
 export PATH="/home/opt/anaconda3/bin:$PATH"
